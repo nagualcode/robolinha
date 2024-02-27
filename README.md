fd3246e234fcdb28da9af9527f1c2303b26ad893
Fred <nagual@nagual.local>
2024-02-27 11:10:00 -0300

    auto

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 ## Robo seguidor de linha com sensor de IR e Ultrassom
 ### Projeto de Bloco de Robótica - INFNET
 
+![Esquema](tinkercad.jpg)
 
+[https://www.tinkercad.com/things/elk2yoNNw5u-robo-seguidor-de-linha-com-sensores-ir-e-ultrassom](https://www.tinkercad.com/things/elk2yoNNw5u-robo-seguidor-de-linha-com-sensores-ir-e-ultrassom)
--- arduino.cpp
+++ arduino.cpp
@@ -5,6 +5,8 @@
 
 const int sensorEsquerdo = 8; // Pino do sensor IR esquerdo
 const int sensorDireito = 9; // Pino do sensor IR direito
+const int trigPin = 10; // Pino do trigger do sensor de ultrassom
+const int echoPin = 11; // Pino do echo do sensor de ultrassom
 
 const int transistorEsquerdo = 6; // Pino do transistor do motor esquerdo
 const int transistorDireito = 5; // Pino do transistor do motor direito
@@ -25,7 +27,8 @@ void setup() {
   pinMode(sensorDireito, INPUT); 
   pinMode(transistorEsquerdo, OUTPUT); 
   pinMode(transistorDireito, OUTPUT); 
-
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   pinMode(botaoPin, INPUT_PULLUP); 
   
   Serial.begin(9600); 
@@ -57,12 +60,29 @@ void loop() {
   int leituraEsquerda = digitalRead(sensorEsquerdo);
   int leituraDireita = digitalRead(sensorDireito);
 
- 
+  // Lê a distância do sensor ultrassônico
+  long duracao, distancia;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  duracao = pulseIn(echoPin, HIGH);
+  distancia = (duracao / 2) / 29.1; // Convertendo para centímetros
+  
   if (exibirSensores) {
-    Serial.print("Sensor Esquerdo: ");
-    Serial.println(leituraEsquerda);
-    Serial.print("Sensor Direito: ");
-    Serial.println(leituraDireita);
+    Serial.print("Distancia do objeto: ");
+    Serial.print(distancia);
+    Serial.println(" cm");
+  }
+
+  // Limita a velocidade a 0 se um objeto estiver a 5 cm ou menos
+  if (distancia <= 10) {
+    digitalWrite(transistorEsquerdo, LOW); 
+    digitalWrite(transistorDireito, LOW);
+    Serial.println("Objeto muito próximo, parando o robô");
+    delay(100); // Pequeno atraso antes de verificar novamente
+    return;
   }
 
   // Controla os transistores dos motores com base na leitura dos sensores
